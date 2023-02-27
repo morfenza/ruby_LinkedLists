0f37c989e69224b6cb1bea0e2929998575467fb2
Morfenza <ianlourenco@protonmail.com>
2023-02-27 13:46:12 -0300

    Create linked list class with methods to be implemented

---
+++ lib/linked_list.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative 'node'
+
+# Class defininf the linked lists, which uses nodes
+class LinkedList
+  def append(value); end
+
+  def prepend(value); end
+
+  def size; end
+
+  def head(value); end
+
+  def tail(value); end
+
+  def at(index); end
+
+  def insert_at(value, index); end
+
+  def remove_at(index); end
+
+  def pop; end
+
+  def contains?(value); end
+
+  def find(value); end
+
+  def to_s; end
+end
